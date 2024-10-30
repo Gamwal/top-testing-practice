34141ca27fa4ef5b99dccf5f9fe59cfb80086439
Gamaliel Adun <adungamaliel@gmail.com>
2024-10-30 12:23:05 +0100

    feat: add reverseString and calculate functionality

--- src/index.js
+++ src/index.js
@@ -2,4 +2,23 @@ function capitalize(string) {
   return string.slice(0, 1).toUpperCase() + string.slice(1);
 }
 
-module.exports = capitalize;
+function reverseString(string) {
+  return string.split("").reverse().join("");
+}
+
+const calculator = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+};
+
+module.exports = { capitalize, reverseString, calculator };
