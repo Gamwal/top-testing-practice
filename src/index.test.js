01ac1e03292bcfee5fc4552af573719c43821c42
Gamaliel Adun <adungamaliel@gmail.com>
2024-10-30 12:30:59 +0100

    test: add tests for reverseString and calculate

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-const capitalize = require("./index");
+const { capitalize, reverseString, calculator } = require("./index");
 
 test("Captializes first letter", () => {
   expect(capitalize("gates")).toBe("Gates");
@@ -6,4 +6,28 @@ test("Captializes first letter", () => {
 
 test("Captializes first letter", () => {
   expect(capitalize("board")).toBe("Board");
+});
+
+test("Captializes first letter", () => {
+  expect(capitalize("BoarD")).toBe("BoarD");
+});
+
+test("Reverses a string", () => {
+  expect(reverseString("apple")).toBe("elppa");
+});
+
+test("Calculates sum", () => {
+  expect(calculator.add(5, 3)).toBe(8);
+});
+
+test("Calculates difference", () => {
+  expect(calculator.subtract(5, 3)).toBe(2);
+});
+
+test("Calculates quotient", () => {
+  expect(calculator.divide(6, 3)).toBe(2);
+});
+
+test("Calculates product", () => {
+  expect(calculator.multiply(5, 3)).toBe(15);
 });
