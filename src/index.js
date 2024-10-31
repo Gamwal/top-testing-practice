8347bb31897726993b48470c98c18961e7b0f1e8
Gamaliel Adun <adungamaliel@gmail.com>
2024-10-31 07:56:20 +0100

    feat: add caesarCipher algorithm

--- src/index.js
+++ src/index.js
@@ -21,4 +21,21 @@ const calculator = {
   },
 };
 
-module.exports = { capitalize, reverseString, calculator };
+function caesarCipher(string, num) {
+  const stringArr = [];
+  for (let index = 0; index < string.length; index++) {
+    const char = string.charCodeAt(index);
+    if (char >= 65 && char <= 90) {
+      const newChar = ((char - 65 + num) % 26) + 65;
+      stringArr.push(String.fromCharCode(newChar));
+    } else if (char >= 97 && char <= 122) {
+      const newChar = ((char - 97 + num) % 26) + 97;
+      stringArr.push(String.fromCharCode(newChar));
+    } else {
+      stringArr.push(String.fromCharCode(char));
+    }
+  }
+  return stringArr.join("");
+}
+
+module.exports = { capitalize, reverseString, calculator, caesarCipher };
