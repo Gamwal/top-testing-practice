9e813bb516d1f647c48c623727b3ecbe1d72ca63
Gamaliel Adun <adungamaliel@gmail.com>
2024-10-31 08:17:29 +0100

    test: add one test for analyzeArray algorithm

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,7 @@ const {
   reverseString,
   calculator,
   caesarCipher,
+  analyzeArray,
 } = require("./index");
 
 test("Captializes first letter", () => {
@@ -52,4 +53,13 @@ test("Generates Caesar Cipher", () => {
 
 test("Generates Caesar Cipher", () => {
   expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+test("Finds Average, Min, Max and Length of an array", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
